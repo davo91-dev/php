431cb597961702c53bae29805f92c8a4b78a2e71
davo91-dev <58267247+davo91-dev@users.noreply.github.com>
2020-10-01 17:34:50 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>body { background-color: grace }</style>
+<head>
+</head>
+<font color="green"><h1>My first PHP page</h1></font>
+
+<?php
+echo '<p style="color: RED">
+       This is AWS Elastic Beanstalk WebServer with PHP App</p>';
+echo   '<br>Server IP:' .$_SERVER['SERVER_ADDR'];
+?> 
+
+</body>
+</html>
